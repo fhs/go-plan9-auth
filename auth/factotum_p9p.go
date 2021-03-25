5c025d2bf4272a229e6f100e8cbe908c9bdd4fe3
Fazlul Shahriar <fshahriar@gmail.com>
2021-03-25 01:49:28 -0400

    Remove old license notice from top of files

--- auth/factotum_p9p.go
+++ auth/factotum_p9p.go
@@ -1,7 +1,4 @@
-// Copyright 2020 The go-plan9-auth Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
+//go:build !plan9
 // +build !plan9
 
 package auth
